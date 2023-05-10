3037e847932bf7a5fc1d7d3c9aa906d4b07323cd
mellb26 <102174403+mellb26@users.noreply.github.com>
2023-05-10 11:47:08 +0100

    fixed errors

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ app.get('/api/categories', getCategories)
 app.use((req, res, next) => {
     res.status(404).send({ msg: "404 not found" });
 });
-app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.send(err.status).send({msg: err.msg})
-    }
-})
 
 app.use((err, req, res, next) => {
     if (err.code === '23503') {
